88e826ed21bd87ed3bb6f53ea5d1d844f44403a7
Clinton Brito <clintoncsb@gmail.com>
2023-07-26 19:50:55 -0300

    :building_construction: Refactor: improving app architecture

--- src/app.js
+++ src/app.js
@@ -2,22 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const teams = require('./utils/teams');
+const validateTeam = require('./middlewares/validateTeam');
+const existingId = require('./middlewares/existingId');
+
 app.use(express.json());
 
 let nextId = 3;
-const teams = [
-  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
-  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
-];
-
-const validateTeam = (req, res, next) => {
-  const requiredProperties = ['nome', 'sigla'];
-  if (requiredProperties.every((property) => property in req.body)) {
-    next(); // Chama o próximo middleware
-  } else {
-    res.sendStatus(400); // Ou já responde avisando que deu errado
-  }
-};
 
 app.get('/teams', (req, res) => res.json(teams));
 
@@ -40,28 +31,23 @@ app.post('/teams', validateTeam, (req, res) => {
   res.status(201).json(team);
 });
 
-app.put('/teams/:id', validateTeam, (req, res) => {
+app.put('/teams/:id', existingId, validateTeam, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find((t) => t.id === id);
 
-  if (team) {
-    const index = teams.indexOf(team);
-    const updated = { id, ...req.body };
-    teams.splice(index, 1, updated);
-    res.status(201).json(updated);
-  } else {
-    res.sendStatus(400);
-  }
+  // Não é mais necessário conferir se team existe por conta da validação prévia efetuada pelo middleware
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);
 });
 
-app.delete('/teams/:id', (req, res) => {
+app.delete('/teams/:id', existingId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find((t) => t.id === id);
 
-  if (team) {
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);
-  }
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
   res.sendStatus(204);
 });
 
