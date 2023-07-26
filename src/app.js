cc59d97660b831f3946989afd70a92400785baf9
Clinton Brito <clintoncsb@gmail.com>
2023-07-26 19:30:07 -0300

    :recycle: Refactor: adds validation function as middleware

--- src/app.js
+++ src/app.js
@@ -10,12 +10,21 @@ const teams = [
   { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
 ];
 
-app.get('/teams', (req, res) => res.status(200).json({ teams }));
+const validateTeam = (req, res, next) => {
+  const requiredProperties = ['nome', 'sigla'];
+  if (requiredProperties.every((property) => property in req.body)) {
+    next(); // Chama o próximo middleware
+  } else {
+    res.sendStatus(400); // Ou já responde avisando que deu errado
+  }
+};
+
+app.get('/teams', (req, res) => res.json(teams));
 
 app.get('/teams/:id', (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find((t) => t.id === id);
-  // res.status(200).json(team);
+
   if (team) {
     res.json(team);
   } else {
@@ -23,28 +32,23 @@ app.get('/teams/:id', (req, res) => {
   }
 });
 
-app.post('/teams', (req, res) => {
-  const requiredProperties = ['nome', 'sigla'];
-  
-  if (requiredProperties.every((property) => property in req.body)) {
-    const team = { id: nextId, ...req.body };
-    teams.push(team);
-    nextId += 1;
-    res.status(201).json(team);
-  } else {
-    res.sendStatus(400);
-  }
+// Arranja os middlewares para chamar validateTeam primeiro
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
 });
 
-app.put('/teams/:id', (req, res) => {
+app.put('/teams/:id', validateTeam, (req, res) => {
   const id = Number(req.params.id);
-  const requiredProperties = ['nome', 'sigla'];
   const team = teams.find((t) => t.id === id);
 
-  if (team && requiredProperties.every((property) => property in req.body)) {
+  if (team) {
     const index = teams.indexOf(team);
     const updated = { id, ...req.body };
     teams.splice(index, 1, updated);
+    res.status(201).json(updated);
   } else {
     res.sendStatus(400);
   }
@@ -53,6 +57,7 @@ app.put('/teams/:id', (req, res) => {
 app.delete('/teams/:id', (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find((t) => t.id === id);
+
   if (team) {
     const index = teams.indexOf(team);
     teams.splice(index, 1);
