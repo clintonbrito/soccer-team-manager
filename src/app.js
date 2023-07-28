994539f69ae11e1de6d7343473678217aa689ec5
Clinton Brito <clintoncsb@gmail.com>
2023-07-27 21:27:30 -0300

    :truck: Refactor: including router middleware and inproving structuring

--- src/app.js
+++ src/app.js
@@ -3,99 +3,29 @@ const helmet = require('helmet');
 require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
+
+// require no nosso novo router
+const teamsRouter = require('./routes/teamsRouter');
 
 const app = express();
 
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.static('./images'));
+app.use(express.json());
 app.use(cors());
 
-// Configuramos um limitador para uma taxa máxima de 100 requisições em um intervalo de 15 minutos
-const limiter = rateLimit({
-  max: 100, // número máximo de requisições
-  windowMs: 15 * 60 * 1000, // intervalo de tempo, em milissegundos, para atingir o número máximo de requisições
-  message: 'Muitas requisições originadas deste IP', // mensagem personalizada quando atinge o limit rate
+// monta o router na rota /teams
+app.use('/teams', teamsRouter);
+
+// middlewares para tratar erros
+app.use((err, _req, _res, next) => {
+  console.error(err.stack);
+  next(err);
 });
 
-app.use(limiter);
-
-const teams = require('./utils/teams');
-const apiCredentials = require('./middlewares/apiCredentials');
-const validateTeam = require('./middlewares/validateTeam');
-const existingId = require('./middlewares/existingId');
-
-app.use(express.json());
-app.use(apiCredentials);
-
-let nextId = 3;
-
-// app.use((req, _res, next) => {
-//   console.log('req.method:', req.method);
-//   console.log('req.path:', req.path);
-//   console.log('req.params:', req.params);
-//   console.log('req.query:', req.query);
-//   console.log('req.headers:', req.headers);
-//   console.log('req.body:', req.body);
-//   next();
-// });
-
-app.get('/teams', (req, res) => res.json(teams));
-
-app.get('/teams/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-
-  if (team) {
-    res.json(team);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-// Arranja os middlewares para chamar validateTeam primeiro
-// app.post('/teams', validateTeam, (req, res) => {
-//   const team = { id: nextId, ...req.body };
-//   teams.push(team);
-//   nextId += 1;
-//   res.status(201).json(team);
-// });
-
-app.post('/teams', validateTeam, (req, res) => {
-  if (
-    // confere se a sigla proposta está inclusa nos times autorizados
-    !req.teams.teams.includes(req.body.sigla)
-    // confere se já não existe um time com essa sigla
-    && teams.every((t) => t.sigla !== req.body.sigla)
-  ) {
-    return res.status(422).json({ message: 'Já existe um time com essa sigla' });
-  }
-
-  const team = { id: nextId, ...req.body };
-  teams.push(team);
-  nextId += 1;
-  res.status(201).json(team);
-});
-
-app.put('/teams/:id', existingId, validateTeam, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-
-  // Não é mais necessário conferir se team existe por conta da validação prévia efetuada pelo middleware
-  const index = teams.indexOf(team);
-  const updated = { id, ...req.body };
-  teams.splice(index, 1, updated);
-  res.status(201).json(updated);
-});
-
-app.delete('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-
-  const index = teams.indexOf(team);
-  teams.splice(index, 1);
-  res.sendStatus(204);
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: `Algo deu errado! Mensagem: ${err.message}` });
 });
 
 module.exports = app;
